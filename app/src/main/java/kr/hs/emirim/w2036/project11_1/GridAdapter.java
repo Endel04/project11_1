964b7b46d862bae6338fa146f39ee51332667702
유하람 <w2036@e-mirim.hs.kr>
2021-07-27 10:04:27 +0900

    Edit MainActivity.java(GridView Adapter) Commit

---
+++ app/src/main/java/kr/hs/emirim/w2036/project11_1/GridAdapter.java
@@ -0,0 +1,45 @@
+package kr.hs.emirim.w2036.project11_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+public class GridAdapter extends BaseAdapter {
+    Context context;
+    int[] posterIds = {R.drawable.i01, R.drawable.i02, R.drawable.i03, R.drawable.i04, R.drawable.i05, R.drawable.i06, R.drawable.i07, R.drawable.i08, R.drawable.i09, R.drawable.i10,
+                       R.drawable.i01, R.drawable.i02, R.drawable.i03, R.drawable.i04, R.drawable.i05, R.drawable.i06, R.drawable.i07, R.drawable.i08, R.drawable.i09, R.drawable.i10};
+
+    public GridAdapter(Context context){
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return posterIds.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View View, ViewGroup viewGroup) {
+        ImageView imgv = new ImageView(context);
+        imgv.setLayoutParams(new ViewGroup.LayoutParams(200, 300));
+        imgv.setScaleType(ImageView.ScaleType.FIT_CENTER);
+        imgv.setPadding(5, 5, 5, 5);
+        imgv.setImageResource(posterIds[i]);
+        return imgv;
+    }
+}
