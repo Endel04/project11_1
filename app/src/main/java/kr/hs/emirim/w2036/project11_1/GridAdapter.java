505d7d08dd2a0eff26009685c3a4e3e89cefebaf
유하람 <w2036@e-mirim.hs.kr>
2021-07-27 10:24:58 +0900

    Edit GridAdapter.java(Show Dialog Event) Commit

--- app/src/main/java/kr/hs/emirim/w2036/project11_1/GridAdapter.java
+++ app/src/main/java/kr/hs/emirim/w2036/project11_1/GridAdapter.java
@@ -1,5 +1,6 @@
 package kr.hs.emirim.w2036.project11_1;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
@@ -40,6 +41,22 @@ public class GridAdapter extends BaseAdapter {
         imgv.setScaleType(ImageView.ScaleType.FIT_CENTER);
         imgv.setPadding(5, 5, 5, 5);
         imgv.setImageResource(posterIds[i]);
+
+        final int pos = i;
+        imgv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder dialog = new AlertDialog.Builder(context);
+                View dialogView = View.inflate(context, R.layout.dialog1, null);
+                ImageView imgvLarge = dialogView.findViewById(R.id.imgv_large);
+                imgvLarge.setImageResource(posterIds[pos]);
+                dialog.setTitle("Large Poster");
+                dialog.setIcon(R.drawable.movie);
+                dialog.setView(dialogView);
+                dialog.setNegativeButton("close", null);
+                dialog.show();
+            }
+        });
         return imgv;
     }
 }
